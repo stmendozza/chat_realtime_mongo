2340bca88e424506f9922964f1db23085e33cd99
Cristian Mendoza <stmendozza@gmail.com>
2021-08-17 18:49:00 -0500

    Funcionalidad de envio de msms en tiempo real

---
+++ src/sockets.js
@@ -0,0 +1,58 @@
+module.exports = io => {
+
+  let users = {};
+
+  io.sockets.on('connection', socket => {
+
+    socket.on('new user', (data, cb) => {
+      if (data in users) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        users[socket.nickname] = socket;
+        updateNicknames();
+      }
+    });
+
+    // receive a message a broadcasting
+    socket.on('send message', (data, cb) => {
+      var msg = data.trim();
+
+      if (msg.substr(0, 3) === '/w ') {
+        msg = msg.substr(3);
+        var index = msg.indexOf(' ');
+        if(index !== -1) {
+          var name = msg.substring(0, index);
+          var msg = msg.substring(index + 1);
+          if (name in users) {
+            users[name].emit('whisper', {
+              msg,
+              nick: socket.nickname 
+            });
+          } else {
+            cb('Error! Enter a valid User');
+          }
+        } else {
+          cb('Error! Please enter your message');
+        }
+      } else {
+        io.sockets.emit('new message', {
+          msg,
+          nick: socket.nickname
+        });
+      }
+    });
+
+    socket.on('disconnect', data => {
+      if(!socket.nickname) return;
+      delete users[socket.nickname];
+      updateNicknames();
+    });
+
+    function updateNicknames() {
+      io.sockets.emit('usernames', Object.keys(users));
+    }
+  });
+
+}
