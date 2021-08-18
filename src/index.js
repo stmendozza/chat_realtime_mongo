6cfa49d71d7c965ad9991040d74e0dfcc131d9dd
Cristian Mendoza <stmendozza@gmail.com>
2021-08-17 20:03:47 -0500

    Guardado de chats en mongodb local

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const socketio = require('socket.io');
 const http = require('http');
 const path = require('path');
 
+const mongoose = require('mongoose');
 // initializing server and sockets
 const app = express();
 const server= http.createServer(app);
 const io = socketio.listen(server);
+
+// db conection
+mongoose.connect('mongodb://localhost/chat-database')
+  .then(db => console.log('db is connected'))
+  .catch(err => console.log(err));
 
 // settings 
 app.set('port', process.env.PORT || 3000);
